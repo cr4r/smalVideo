7f40de4ae9ed12e7b45ccd90d4e36f06ca1d647a
cr4r1 <ahmadfatchurrachman@mhs.mdp.ac.id>
2020-10-20 15:42:56 +0000

    update

--- encode.py
+++ encode.py
@@ -4,8 +4,11 @@ mediaa='video/'
 
 for subdirs, dirs, files in os.walk(mediaa):
     for file in files:
+	
         extension = os.path.splitext(file)[-1].lower()
         if extension == ".mp4":
-            mediain = subdirs +"/"+ file
-            mediaout = subdirs +"/"+ 'compressed_'+file
+	    if not.os.path.exists(subdirs+ '/compressed'):
+		os.makedirs(subdirs+ '/compressed')
+            mediain = subdirs +'/'+ file
+            mediaout = subdirs +'/compressed/'+file
             subprocess.run('ffmpeg -i '+ mediain.replace(" ","\\ ") +' -vcodec libx264 -crf 22 ' + mediaout.replace(" ","\\ "), shell=True)
